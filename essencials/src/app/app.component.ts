0b02eed00dc2fb7c5b60f873ba8827d04971cfcd
Alexandre Bueno da Silva <abueno.silva@gmail.com>
2024-08-02 13:31:48 -0300

    Atualizações

--- essencials/src/app/app.component.ts
+++ essencials/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { CommonModule } from '@angular/common';
 import { TasksComponent } from './tasks/tasks.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -21,7 +21,7 @@ import { TasksComponent } from './tasks/tasks.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
