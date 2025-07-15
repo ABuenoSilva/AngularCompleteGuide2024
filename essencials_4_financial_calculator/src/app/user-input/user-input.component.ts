3cb1aaa551c256caa5199f2718a57aadbe23dca8
Alexandre Bueno da Silva <tp.alexandre.bueno@ext.totvs.com.br>
2025-07-15 14:22:14 -0300

    Finalização sessão 4

--- essencials_4_financial_calculator/src/app/user-input/user-input.component.ts
+++ essencials_4_financial_calculator/src/app/user-input/user-input.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  initialInvestment = signal(0);
+  annualInvestment = signal(0);
+  expectedReturn = signal(5);
+  duration = signal(10);
 
+  constructor(private investmentService: InvestmentService) {}
+
+  calculateInvestmentResults() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: this.initialInvestment(),
+      annualInvestment: this.annualInvestment(),
+      expectedReturn: this.expectedReturn(),
+      duration: this.duration()
+    });
+    this.initialInvestment.set(0);
+    this.annualInvestment.set(0);
+    this.expectedReturn.set(5);
+    this.duration.set(10);
+  }
 }
